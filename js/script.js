25b86910a18d3dff4bcb58bb8f36b60750649eae
Pawel Klasa <pawel.klasa@mac000181.opaltelecom.com>
2017-01-16 16:31:57 +0000

    drop down

--- js/script.js
+++ js/script.js
@@ -57,3 +57,16 @@ function emailVerify(){
 		return true;
 	}
 }
+
+// DROPDOWN TUTORIAL
+
+function toggleNavPanel(x){
+    var panel = document.getElementById(x), navarrow = document.getElementById("navarrow"), maxH="300px";
+    if(panel.style.height == maxH){
+        panel.style.height = "0px";
+        navarrow.innerHTML = "&#9662;";
+    } else {
+        panel.style.height = maxH;
+        navarrow.innerHTML = "&#9652;";
+    }
+}
